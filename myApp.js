07daa107d50372040702bc396ddbe176480a846b
VictorChukwudi <victorukay@gmail.com>
2023-11-01 14:46:12 +0100

    content-security-policy

--- myApp.js
+++ myApp.js
@@ -11,6 +11,10 @@ app.use(helmet.ieNoOpen())
 app.use(helmet.hsts({maxAge:ninetyDays, force:true}))
 app.use(helmet.dnsPrefetchControl())
 app.use(helmet.noCache())
+app.use(helmet.contentSecurityPolicy({directives:{
+  defaultSrc:["'self'"],
+  scriptSrc:["self",'trusted-cdn.com']
+}}))
 
 
 
