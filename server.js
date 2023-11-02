0eb601c0d10298ac8cad67d62475e58a5214844b
VictorChukwudi <victorukay@gmail.com>
2023-11-02 12:42:36 +0100

    bcrypt

--- server.js
+++ server.js
@@ -67,13 +67,13 @@ app.use(function (req, res, next) {
 });
 
 
-bcrypt.hash('passw0rd!', 13, (err, hash) => {
-  console.log(hash);
-  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-  bcrypt.compare('passw0rd!', hash, (err, res) => {
-    console.log(res); //true
-  });
-});
+// bcrypt.hash('passw0rd!', 13, (err, hash) => {
+//   console.log(hash);
+//   //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+//   bcrypt.compare('passw0rd!', hash, (err, res) => {
+//     console.log(res); //true
+//   });
+// });
 
 module.exports = app;
 
